e6a0c4b52ab49b14ecaa31014f6c02861d981b01
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-19 08:59:41 -0500

    changed format of connection

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,13 @@
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
-
-// Require mysql
 var mysql = require("mysql");
-var connection;
 
-if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else{
-
-// Set up our connection information
-connection = mysql.createConnection({
-  port: 3306,
+var connection = mysql.createConnection({
   host: "localhost",
+  port: 3306,
   user: "root",
   password: "testtest",
-  database: "burgers_db"
+  database: "pets_db"
 });
-}
 
-// Connect to the database
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -30,5 +16,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection
 module.exports = connection;
