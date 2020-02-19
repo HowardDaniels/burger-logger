a4d0a2af9cba2d8a4d062b4df31b955329f27a9a
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-19 09:00:26 -0500

    changed database name

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "testtest",
-  database: "pets_db"
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
