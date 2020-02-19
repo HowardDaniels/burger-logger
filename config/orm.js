2026150683d11aba5c4ab67aa205b028e6241972
Howard Daniels <hdaniels13tat@gmail.com>
2020-02-19 13:16:42 -0500

    updated ORM methods

--- config/orm.js
+++ config/orm.js
@@ -7,28 +7,28 @@ var connection = require("./connection.js");
 // These help avoid SQL injection
 // https://en.wikipedia.org/wiki/SQL_injection
 var orm = {
-  selectWhere: function(tableInput, colToSearch, valOfCol) {
-    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+  selectAll: function(tableInput) {
+    var queryString = "SELECT * FROM ??";
+    connection.query(queryString, [tableInput], function(err, result) {
       if (err) throw err;
       console.log(result);
     });
   },
-  selectAndOrder: function(whatToSelect, table, orderCol) {
-    var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
+  insertOne: function(tableInput, colTwoVal, colThreeVal) {
+    var queryString = "INSERT INTO ?? VALUES (?, ?)";
     console.log(queryString);
-    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+    connection.query(queryString, [tableInput, colTwoVal, colThreeVal], function(err, result) {
       if (err) throw err;
       console.log(result);
     });
   },
-  findWhoHasMost: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
+  updateOne: function(table, col, tableInput, col, colVal) {
     var queryString =
-      "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
+      "UPDATE ?? SET ?? = ? WHERE ?? = ?";
 
     connection.query(
       queryString,
-      [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
+      [table, col, tableInput, col, colVal],
       function(err, result) {
         if (err) throw err;
         console.log(result);
